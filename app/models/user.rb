3cddb2b103bd7220860bc8a553304cdc1f005c7e
suzukida <give.me.wing1020@gmail.com>
2021-09-26 22:32:49 +0900

    ユーザー認証機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true
+         validates :first_name, presence: true
+         validates :first_kana, presence: true
+         validates :last_name, presence: true
+         validates :last_kana, presence: true
+         validates :birthday, presence: true
+
+         has_many :items
+         has_many :buy_items
+         
 end
