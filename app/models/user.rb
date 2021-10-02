d10c9fd40222efc2fa95472e8c111ec9d68208b3
suzukida <give.me.wing1020@gmail.com>
2021-10-03 00:43:13 +0900

    プルダウン作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
   validates :name, presence: true
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-  # has_many :items
+  has_many :items
   # has_many :buy_items
 end
