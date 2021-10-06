6523574076a5622a563681d9deaff96bf5136c44
suzukida <give.me.wing1020@gmail.com>
2021-10-06 18:31:29 +0900

    商品情報機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :contributor_confirmation, only: [:edit, :update]
   def index
     @item = Item.includes(:user)
   end
@@ -21,10 +22,26 @@ class ItemsController < ApplicationController
     @item =Item.find(params[:id])
   end
 
+  def edit
+    @item =Item.find(params[:id])
+  end
+
+  def update
+    @item =Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image,:item_name, :explanation, :category_id, :condition_id,:shipping_id,:prefecture_id,:delivery_id,:item_price).merge(user_id: current_user.id)
-    # params[:item].permit(:image,:item_name, :explanation, :category_id, :condition_id,:shipping_id,:send_id,:delivery_id,:item_price)
   end
-  
+    def contributor_confirmation
+      @item =Item.find(params[:id])
+      redirect_to root_path unless @item.user == current_user
+    end
 end
+
